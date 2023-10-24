66e5152aa7dbd05a4834ce78cb7ad87bbd47abe4
Earth <earthlono123@gmail.com>
2023-10-25 02:26:51 +1300

    Fix bug by changing to post method (Refresh token)

--- src/apis/auth.api.js
+++ src/apis/auth.api.js
@@ -3,4 +3,4 @@ import { apiInstance } from "configs/axios.config";
 export const registerApi = (data) => apiInstance.post("/auth/register", data);
 export const loginApi = (data) => apiInstance.post("/auth/login", data);
 export const logoutApi = () => apiInstance.post("/auth/logout");
-export const refreshToken = (token) => apiInstance.get("/auth/refresh_token",token);
+export const refreshToken = (token) => apiInstance.post("/auth/refresh_token",token);
